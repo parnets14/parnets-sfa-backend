47c607f9d0e6f469f68e6b0564f7762db92582f5
Shubhamchy12 <shubc321@gmail.com>
2026-07-20 09:27:16 +0530

    qwerty

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
